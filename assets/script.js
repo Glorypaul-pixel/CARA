27a9d1b2115e205a80e7bb1353b83a3cfd2be034
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-05-18 14:40:22 +0100

    i think we are done

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,3 @@
-let script = window.location.pathname;
-let headLink = document.querySelectorAll("ul a").forEach(link =>{
-    if(link.href.includes(`${script}`)){
-        link.classList.add('active');
-    }
-});
 const menuIcon = document.querySelector('#menu-Icon');
 const navbar = document.querySelector('.menu');
 
@@ -12,41 +6,38 @@ menuIcon.addEventListener ('click', () => {
     navbar.classList.toggle('active');
 });
 
-
-
 // darkmode
-const toggleIcon = document.querySelector('.toggle-icon');  
+// const toggleIcon = document.querySelector('.toggle-icon');  
 
-toggleIcon.addEventListener('click', () => {
-    toggleIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark-mode');
-});
+// toggleIcon.addEventListener('click', () => {
+//     toggleIcon.classList.toggle('bx-sun');
+//     document.body.classList.toggle('dark-mode');
+// });
 
 //=== scroll section active links
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelectorAll('header nav   a');
 
 window.onscroll = () => {
-    sections.forEach(sec =>{
-        const top = window.scrollY;
-        const offset = sec.offsetTop - 150;
-        const height = sec.offsetHeight;
-        const id = sec.getAttribute('id');
+    // sections.forEach(sec =>{
+    //     const top = window.scrollY;
+    //     const offset = sec.offsetTop - 150;
+    //     const height = sec.offsetHeight;
+    //     const id = sec.getAttribute('id');
     
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active')
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            })
-        };
-    })
-// sticky header
+    //     if(top >= offset && top < offset + height) {
+    //         navLinks.forEach(links => {
+    //             links.classList.remove('active')
+    //             document.querySelector('header ul a[href*=' + id + ']').classList.add('active')
+    //         })
+    //     };
+    // })
 
+// sticky header
     const header = document.querySelector('.header');
     header.classList.toggle('sticky' , window.scrollY > 100);
 
     // remove navbar when you click the body
-    // document.body.classList.remove('bx-x active');
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
